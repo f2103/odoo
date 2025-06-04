f70627a896b22707cce311b3e087d56a389d1b97
f2103 <150715675+f2103@users.noreply.github.com>
2025-06-04 09:56:50 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,17 @@ while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done
 
 echo Database is now available
 
+if [ -n "$OCA_REPOS" ]; then
+  mkdir -p /mnt/extra-addons
+  IFS=',' read -ra repos <<< "$OCA_REPOS"
+  for r in "${repos[@]}"; do
+    echo "→ Klone OCA/$r"
+    git clone --depth 1 --branch 18.0 https://github.com/OCA/$r.git /mnt/extra-addons/$r
+  done
+  chown -R odoo:odoo /mnt/extra-addons
+  export ODOO_ADDONS_PATH="/usr/lib/python3/dist-packages/odoo/addons,/mnt/extra-addons"
+fi
+
 exec odoo \
     --http-port="${PORT}" \
     --init=all \
