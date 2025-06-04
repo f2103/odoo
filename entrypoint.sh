8e8d7877733413c744effb3ae0803933a6f8cbf5
f2103 <150715675+f2103@users.noreply.github.com>
2025-06-04 11:29:07 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-echo Waiting for database...
-
-while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done; 
-
-echo Database is now available
+echo "Waiting for database..."
+while ! nc -z "${ODOO_DATABASE_HOST}" "${0DOO_DATABASE_PORT}" 2>&1; do sleep 1; done;
+echo "Database is now available"
 
 exec odoo \
     --http-port="${PORT}" \
@@ -22,4 +20,5 @@ exec odoo \
     --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
     --smtp-user="${ODOO_SMTP_USER}" \
     --smtp-password="${ODOO_SMTP_PASSWORD}" \
-    --email-from="${ODOO_EMAIL_FROM}" 2>&1
+    --email-from="${ODOO_EMAIL_FROM}" \
+    --addons-path="/opt/odoo/addons,/opt/odoo/custom_addons/server-tools,/opt/odoo/custom_addons/mis-builder,/opt/odoo/custom_addons/web,/opt/odoo/custom_addons/account-financial-tools,/opt/odoo/custom_addons/project,/opt/odoo/custom_addons/resource_booking,/opt/odoo/custom_addons/hr,/opt/odoo/custom_addons/timesheet,/opt/odoo/custom_addons/purchase-workflow,/opt/odoo/custom_addons/account-invoicing,/opt/odoo/custom_addons/account-financial-reporting,/opt/odoo/custom_addons/account-reconcile,/opt/odoo/custom_addons/account-payment" 2>&1
